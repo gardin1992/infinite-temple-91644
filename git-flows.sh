f9014e89f1f4e2d3ef2d72de6ce12b126e83c6f9
Joao Antonio Gardin Vieira <gardin1992@gmail.com>
2017-12-31 14:19:47 -0200

    s

--- git-flows.sh
+++ git-flows.sh
@@ -2,3 +2,8 @@
 
 git -c diff.mnemonicprefix=false -c core.quotepath=false -c credential.helper=sourcetree add -f -- README.md
 
+git -c diff.mnemonicprefix=false -c core.quotepath=false -c credential.helper=sourcetree commit -q -F /var/folders/1k/dzz0q0wn175cj9r67mtdp9l00000gn/T/SourceTreeTemp.ALRymv
+
+git -c diff.mnemonicprefix=false -c core.quotepath=false -c credential.helper=sourcetree push -v --tags origin refs/heads/develop:refs/heads/develop
+
+git -c diff.mnemonicprefix=false -c core.quotepath=false -c credential.helper=sourcetree stash save teste 
